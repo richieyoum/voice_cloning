1c756eb2567e92e34c93dcd6082a33ba6b8d9287
Richie Youm <seongmin.youm@mail.mcgill.ca>
2022-03-09 00:29:00 -0500

    replace requirements.txt with environment.yml

--- setup.sh
+++ setup.sh
@@ -1,2 +1,12 @@
-pip install -r requirements.txt
 sudo apt-get update && sudo apt-get install -y libsndfile1
+
+if conda env list | grep -q voice_cloning; then
+    echo voice_cloning environment already exists! 
+else
+    echo creating new environment
+    conda env create -f environment.yml
+fi
+
+echo activating voice_cloning environment...
+conda activate voice_cloning
+echo done!
