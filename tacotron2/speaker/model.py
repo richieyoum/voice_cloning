09b4efc46c7e8aac1ff083121e61e02a18e5baf6
Richie Youm <syoum@wealthsimple.com>
2022-03-17 16:18:51 -0400

    initial implementation of SpeakerEncoder forward function

--- tacotron2/speaker/model.py
+++ tacotron2/speaker/model.py
@@ -5,11 +5,49 @@ import numpy as np
 import torch
 
 class SpeakerEncoder(nn.Module):
+    """ Learn speaker representation from speech utterance of arbitrary lengths.
+    """
     def __init__(self, device, loss_device):
-        pass
+        super().__init__()
+        self.loss_device = loss_device
 
-    def forward(self, utterances, hidden_init=None):
-        pass
+        # initial lstm takes 40 channel log-mel spectrograms, projected to 256 dimensions
+        self.lstm1 = nn.LSTM(
+            input_size=40,
+            hidden_size=256,
+            num_layers=1,
+            batch_first=True,
+            dropout=0,
+            bidirectional=False,
+            proj_size=256
+        ).to(device)
+        # other two lstms
+        self.lstm2_3 = nn.LSTM(
+            input_size=256,
+            hidden_size=256,
+            num_layers=2,
+            batch_first=True,
+            dropout=0,
+            bidirectional=False,
+            proj_size=256
+        ).to(device)
+        self.linear = nn.Linear(in_features=256, out_features=256).to(device)
+        self.relu = nn.ReLU().to(device)
+        # epsilon term for numerical stability ( ie - division by 0)
+        self.epsilon = 1e-5
+
+    def forward(self, utterances, h_init=None, c_init=None):
+        # implement section 2.1 from https://arxiv.org/pdf/1806.04558.pdf
+        out, (hidden, cell) = self.lstm1(utterances, (h_init, c_init))
+        out, (hidden, cell) = self.lstm2_3(out, (hidden, cell))
+
+        # compute speaker embedding from hidden state of final layer
+        final_hidden = hidden[-1]
+        speaker_embedding = self.relu(self.linear(final_hidden))
+
+        # l2 norm of speaker embedding
+        speaker_embedding = speaker_embedding / (torch.norm(speaker_embedding, dim=1, keepdim=True) + self.epsilon)
+        return speaker_embedding
 
     def gradient_clipping():
         pass
